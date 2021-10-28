4f0d90f7505cd9f10e50cbd8bdb61048e52e33bd
Your Name <you@example.com>
2021-10-28 15:06:43 +0400

    refactor methods

--- src/main/java/com/example/restservice/RestController.java
+++ src/main/java/com/example/restservice/RestController.java
@@ -5,11 +5,10 @@ import com.example.restservice.service.ServiceOpenexchangerates;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 
-@RestController
-public class Controller {
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
 
     @Autowired
     ServiceOpenexchangerates serviceOpen;
@@ -18,16 +17,18 @@ public class Controller {
     ServiceGiphy serviceGiphy;
 
     @GetMapping(path = "/get")
-    public String getRes() throws URISyntaxException {
+    public String getResult() throws URISyntaxException {
         String gif = "";
-        switch (serviceOpen.getd()) {
-            case 1:
+        switch (serviceOpen.getLatestChanges()) {
+            case "rich":
                 gif = serviceGiphy.getRich();
                 break;
-            case -1:
+            case "broke":
                 gif = serviceGiphy.getBroke();
                 break;
+            default:
+                gif = "Nothing";
         }
-        return "<img src=\"" + gif + "\" style=\"width:600px;height:400px\" alt=\"The Logo\">";
+        return "<img src=\"" + gif + "\" style=\"width:500px;height:400px\" alt=\"The Logo\">";
     }
 }
--- src/main/java/com/example/restservice/client/OpenexchangeratesClient.java
+++ src/main/java/com/example/restservice/client/OpenexchangeratesClient.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.time.LocalDate;
 
-
-
 @FeignClient(name = "restClient", url = "${openexchangerates.url}")
 public interface OpenexchangeratesClient {
 
@@ -17,9 +15,7 @@ public interface OpenexchangeratesClient {
             consumes = "application/json")
     OpenexchangeModel getLatest(@RequestParam("app_id") String appId, @RequestParam("") String base, @RequestParam("") String symbols);
 
-    @GetMapping(path = "/historical/${data}.json", consumes = "application/json")
-    OpenexchangeModel getHistorical(@RequestParam("app_id") String appId, @RequestParam("") String base, @RequestParam("") String symbols);
+    @GetMapping(path = "/historical/{data}.json", consumes = "application/json")
+    OpenexchangeModel getHistorical(@PathVariable("") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate data, @RequestParam("app_id") String appId, @RequestParam("") String base, @RequestParam("") String symbols);
 
-    @GetMapping(path = "/historical/{yesterday}.json", consumes = "application/json")
-    OpenexchangeModel getYesterday(@PathVariable("") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate yesterday, @RequestParam("app_id") String appId, @RequestParam("") String base, @RequestParam("") String symbols);
 }
