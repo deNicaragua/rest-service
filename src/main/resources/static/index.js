60ae3f764f72264e819f48939f6a13df80cecb1f
Your Name <you@example.com>
2021-10-28 15:08:14 +0400

    add tests

--- src/main/resources/static/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        type: "GET",
-        url: "http://localhost:8080/get",
-        dataType: "text"
-        }).then(function(data) {
-            $('.rate').append(data.Id);
-        });
-});
