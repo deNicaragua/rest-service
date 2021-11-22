6058786e23cdd10011de2e2a66c2ea5cd8efd9a5
Your Name <you@example.com>
2021-11-22 15:20:44 +0400

    formatted giphyclient

--- src/main/java/com/example/restservice/client/GiphyClient.java
+++ src/main/java/com/example/restservice/client/GiphyClient.java
@@ -5,8 +5,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Map;
-
 @FeignClient(name = "client", url = "${giphy.url}")
 public interface GiphyClient {
 
